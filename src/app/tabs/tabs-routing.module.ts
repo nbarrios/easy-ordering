229592d01ae4e19d683df83011d6462a25486454
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2021-10-20 00:36:00 -0700

    Minor update

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'signup',
         loadChildren: () => import('../signup/signup.module').then(m => m.SignupPageModule)
       },
       {
@@ -21,7 +21,7 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/signup',
         pathMatch: 'full'
       }
     ]
