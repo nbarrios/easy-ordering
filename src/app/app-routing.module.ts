0d6fc011458ad3da12c4617bc02d556f78a6f0cd
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2021-10-19 21:02:07 -0700

    Revert "Merge branch 'main' into Megan"
    This reverts commit f4546793a77ba40cc7f61d52eedc2792cad1d27e, reversing
    changes made to e27a5acffd9763eac93fe86774eb43d8004db9c9.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'new-order',
-    loadChildren: () => import('./new-order/new-order.module').then( m => m.NewOrderPageModule)
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
