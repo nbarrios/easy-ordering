a3526734f9396aaa372c7e27dda6449e4924e5a4
M-cloud123 <68924164+M-cloud123@users.noreply.github.com>
2021-10-19 23:52:50 -0700

    Login and Sign Up Layouts
    Basic account sign-in layouts

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('../signup/signup.module').then(m => m.SignupPageModule)
       },
       {
         path: 'tab2',
