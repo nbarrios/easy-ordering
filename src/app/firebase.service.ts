bf0156e47e36767c4541eba79afaa43f2e7defa1
Nick Barrios <nick@nicholasbarrios.com>
2021-11-17 02:27:52 -0800

    Keep a reference to the current user, either at login or via subscribing to the user through AngularFire. Move Google and Firebase oauth login functions into firebaseservice.

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
 
 export class FirebaseUserData {
   uid: string;
@@ -18,17 +19,71 @@ export class FirebaseUserData {
   providedIn: 'root',
 })
 export class FirebaseService {
+  private user: firebase.User = null;
+
   constructor(
     public firestore: AngularFirestore,
     public auth: AngularFireAuth
-  ) {}
+  ) {
+    this.auth.user.subscribe(val => {
+      this.user = val;
+    });
+  }
+
+  getUserID() {
+    if (this.user) {
+      return this.user.uid;
+    } else {
+      return null;
+    }
+  }
 
   loginWithAccount(data: FirebaseUserData) {
-    return this.auth.signInWithEmailAndPassword(data.email, data.password);
+    return new Promise<firebase.auth.UserCredential>((resolve, reject) => {
+      this.auth.signInWithEmailAndPassword(data.email, data.password)
+        .then(credential => {
+          this.user = credential.user;
+          resolve(credential);
+        }, err => {
+          reject(err);
+        });
+    });
+  }
+
+  loginWithGoogle() {
+    return new Promise<firebase.auth.UserCredential>((resolve, reject) => {
+      this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+        .then(credential => {
+          this.user = credential.user;
+          resolve(credential);
+        }, err => {
+          reject(err);
+        });
+    });
+  }
+
+  loginWithFacebook() {
+    return new Promise<firebase.auth.UserCredential>((resolve, reject) => {
+      this.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+        .then(credential => {
+          this.user = credential.user;
+          resolve(credential);
+        }, err => {
+          reject(err);
+        });
+    });
   }
 
   signup(data: FirebaseUserData) {
-    return this.auth.createUserWithEmailAndPassword(data.email, data.password);
+    return new Promise<firebase.auth.UserCredential>((resolve, reject) => {
+      this.auth.createUserWithEmailAndPassword(data.email, data.password)
+        .then(credential => {
+          this.user = credential.user;
+          resolve(credential);
+        }, err => {
+          reject(err);
+        });
+    });
   }
 
   saveDetails(data: FirebaseUserData) {
