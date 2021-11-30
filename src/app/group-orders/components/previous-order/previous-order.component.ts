14dc277b8a12ce9e7558c2a130920c8d950ef88b
Nick Barrios <nick@nicholasbarrios.com>
2021-11-29 20:00:38 -0800

    Update previous order list to use GroupOrder and listen for changes from Firebase.

--- src/app/group-orders/components/previous-order/previous-order.component.ts
+++ src/app/group-orders/components/previous-order/previous-order.component.ts
@@ -1,20 +1,25 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
-import { Order } from '../../models/Order';
-import { OrderStatus } from '../../models/Members';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FirebaseUserData } from 'src/app/firebase.service';
+import { GroupOrder } from '../../models/GroupOrder';
 
 @Component({
   selector: 'app-previous-order',
   templateUrl: './previous-order.component.html',
   styleUrls: ['./previous-order.component.scss'],
 })
-export class PreviousOrderComponent {
+export class PreviousOrderComponent implements OnInit {
+  @Input() order: GroupOrder;
+  @Input() userMap: Map<string, string> = new Map();
 
+  constructor(public firestore: AngularFirestore) {}
 
-  //@Output() showRequest: EventEmitter<OrderDetail> = new EventEmitter<OrderDetail>();
-  @Input() order: Order;
-
-
-  /*showOrder(){
-      this.showRequest.emit(this.order);
-  }*/
+  ngOnInit() {
+    for (const order of this.order.orders) {
+      this.firestore.collection<FirebaseUserData>('users').doc(order.user)
+        .valueChanges().subscribe(val => {
+          this.userMap.set(order.user, (val.name && val.name.length > 0) ? val.name : val.email);
+        });
+    }
+  }
 }
