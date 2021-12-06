c6ab2560562e7a9e0f70805d1bb3ab20e509e394
Nick Barrios <nick@nicholasbarrios.com>
2021-12-05 22:31:53 -0800

    firebase service: remove old console log.

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -25,7 +25,6 @@ export class FirebaseService {
     public auth: AngularFireAuth
   ) {
     this.auth.user.subscribe(val => {
-      console.log('Received user');
       this.user = val;
     });
   }
